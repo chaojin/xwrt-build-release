1477316bf2c9a4f1e8abbf3435f203a295f02450
chaojin <chao_jin@hotmail.com>
2026-09-01 19:05:54 +0800

    release: 26.04_b202608290953=mediatek-filogic
    Signed-off-by: chaojin <chao_jin@hotmail.com>

--- build.sh
+++ build.sh
@@ -12,8 +12,6 @@ echo starting build...
 echo starting build....
 df -h .
 free -m
-echo start build in 10s
-sleep 10
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
